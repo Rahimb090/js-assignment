2a0fe2a052550f8145489243f261b9d2d3ab2d52
Rahimb090 <rahimbaig2020@gmail.com>
2025-10-30 06:16:48 +0000

    Add files via upload

---
+++ js assignment 01/app.js
@@ -0,0 +1,24 @@
+// Assignment 01 
+//----- Chapter 01------
+
+// Alert
+
+
+// Q1 & 02)
+  alert("Error! Please enter a valid password")
+// Q3)
+  alert("Welcome to js land... \n Happy coding! ")
+
+// Q4)
+ alert("Hello Well come to my website")
+
+// Q4)
+ alert("Welcome to js land...")
+ 
+
+ // Q4)
+ alert("Happy coding!")
+confirm("Prevent this page from creating addotional dialogs")
+// Q5)
+ alert("Hello... I can run js through my web browser's console ")
+ 
